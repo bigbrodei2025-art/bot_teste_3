63d5356dd05d9c106ea3994c318418be829d10cb
bigbrodei2025-art <bigbrodei2025@gmail.com>
2025-09-22 02:52:22 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // === CONFIGURAÇÃO DO BOT ===
 module.exports = {
   PREFIX: "!",
-  ADMIN_JIDS: ["55219xxxxxxxx@s.whatsapp.net"],
+  ADMIN_JIDS: ["5551985156039@s.whatsapp.net"],
 
   // Grupo que o bot vai monitorar (coloque o JID real do grupo)
   MONITORED_GROUP_JID: "120363028642401363@g.us",
